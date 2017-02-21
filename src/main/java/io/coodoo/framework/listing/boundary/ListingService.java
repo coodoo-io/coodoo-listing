579ad8c8fdecbacc8f2bef0eac61d7ee2f89e64d
Markus Kühle <markus.kuehle@coodoo.de>
2017-02-21 13:43:49 +0100

    Generic Types only method wide

--- src/main/java/io/coodoo/framework/listing/boundary/ListingService.java
+++ src/main/java/io/coodoo/framework/listing/boundary/ListingService.java
@@ -9,42 +9,36 @@ import javax.persistence.PersistenceContext;
 import io.coodoo.framework.listing.control.ListingFilterQuery;
 
 @Stateless
-public class ListingService<T> {
+public class ListingService {
 
     @PersistenceContext
     EntityManager entityManager;
 
 
-    public ListingResult<T> getListingResult(Class<T> entityClass, ListingQueryParams queryParams) {
-
+    public <T> ListingResult<T> getListingResult(Class<T> entityClass, ListingQueryParams queryParams) {
         return new ListingResult<T>(getListing(entityClass, queryParams), new Metadata(countListing(entityClass, queryParams), queryParams));
     }
 
-    public List<T> getListing(Class<T> entityClass, ListingQueryParams queryParams) {
-
+    public <T> List<T> getListing(Class<T> entityClass, ListingQueryParams queryParams) {
         return new ListingFilterQuery<>(entityManager, entityClass).sort(queryParams.getSortAttribute(), queryParams.isSortAsc())
                         .filterAllAttributes(queryParams.getFilter()).filterByAttributes(queryParams.getFilterAttributes())
                         .list(queryParams.getIndex(), queryParams.getLimit());
     }
 
-    public Long countListing(Class<T> entityClass, ListingQueryParams queryParams) {
-
+    public <T> Long countListing(Class<T> entityClass, ListingQueryParams queryParams) {
         return new ListingFilterQuery<>(entityManager, entityClass).sort(queryParams.getSortAttribute(), queryParams.isSortAsc())
                         .filterAllAttributes(queryParams.getFilter()).filterByAttributes(queryParams.getFilterAttributes()).count();
     }
 
-    public ListingResult<T> getListingResult(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
-
+    public <T> ListingResult<T> getListingResult(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
         return getListingResult(entityClass, new ListingQueryParams(limit, page, index, sortAttribute, filter));
     }
 
-    public List<T> getListing(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
-
+    public <T> List<T> getListing(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
         return getListing(entityClass, new ListingQueryParams(limit, page, index, sortAttribute, filter));
     }
 
-    public Long countListing(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
-
+    public <T> Long countListing(Class<T> entityClass, Integer limit, Integer page, Integer index, String sortAttribute, String filter) {
         return countListing(entityClass, new ListingQueryParams(limit, page, index, sortAttribute, filter));
     }
 
