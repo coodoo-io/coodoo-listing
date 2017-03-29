27bbef647a5a4f86a4f18d7a777cef3ca037a27b
Arend Kühle <arend.kuehle@coodoo.de>
2017-03-29 11:58:58 +0200

    Hoodoo prefix on the Service

---
+++ src/main/java/io/coodoo/framework/listing/boundary/CoodooListingService.java
@@ -0,0 +1,54 @@
+package io.coodoo.framework.listing.boundary;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+import io.coodoo.framework.listing.boundary.annotation.CoodooListingDatabase;
+
+@Stateless
+public class CoodooListingService {
+
+    @Inject
+    @CoodooListingDatabase
+    EntityManager entityManager;
+
+    public <T> ListingResult<T> getListingResult(Class<T> entityClass, ListingQueryParams queryParams) {
+        return Listing.getListingResult(entityManager, entityClass, queryParams);
+    }
+
+    public <T> List<T> getListing(Class<T> entityClass, ListingQueryParams queryParams) {
+        return Listing.getListing(entityManager, entityClass, queryParams);
+    }
+
+    public <T> Long countListing(Class<T> entityClass, ListingQueryParams queryParams) {
+        return Listing.countListing(entityManager, entityClass, queryParams);
+    }
+
+    public <T> ListingResult<T> getListingResult(Class<T> entityClass, Integer page, Integer limit) {
+        return Listing.getListingResult(entityManager, entityClass, page, limit);
+    }
+
+    public <T> List<T> getListing(Class<T> entityClass, Integer page, Integer limit) {
+        return Listing.getListing(entityManager, entityClass, page, limit);
+    }
+
+    public <T> Long countListing(Class<T> entityClass, Integer page, Integer limit) {
+        return Listing.countListing(entityManager, entityClass, page, limit);
+    }
+
+    public <T> ListingResult<T> getListingResult(Class<T> entityClass, Integer page, Integer limit, String sortAttribute) {
+        return Listing.getListingResult(entityManager, entityClass, page, limit, sortAttribute);
+    }
+
+    public <T> List<T> getListing(Class<T> entityClass, Integer page, Integer limit, String sortAttribute) {
+        return Listing.getListing(entityManager, entityClass, page, limit, sortAttribute);
+    }
+
+    public <T> Long countListing(Class<T> entityClass, Integer page, Integer limit, String sortAttribute) {
+        return Listing.countListing(entityManager, entityClass, page, limit, sortAttribute);
+    }
+
+}
